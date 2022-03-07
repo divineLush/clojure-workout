c52926ede09034e992d1ccb444062af632c9348c
divineLush <alvipath@yahoo.com>
2022-03-07 12:08:09 -0500

    init

---
+++ eratosphenes.clj
@@ -0,0 +1,25 @@
+(def n (int (read)))
+(def nums (range 2 (inc n)))
+
+(defn notMult [nm dv]
+  (not (zero? (mod nm dv)))
+)
+
+(defn sieve [ls nm]
+  (filter
+    (fn [el] (notMult el nm))
+    ls
+  )
+)
+
+(defn eratosphenes [candidates]
+  (if (not (empty? candidates))
+    (
+      (def curPrime (first candidates))
+      (println curPrime)
+      (eratosphenes (sieve candidates curPrime))
+    )
+  )
+)
+
+(eratosphenes nums)
