ba835e2a8b223d8065d39373b966908e5b49af62
divineLush <alvipath@yahoo.com>
2022-03-07 12:11:20 -0500

    nicer formatting

--- eratosphenes.clj
+++ eratosphenes.clj
@@ -2,24 +2,17 @@
 (def nums (range 2 (inc n)))
 
 (defn notMult [nm dv]
-  (not (zero? (mod nm dv)))
-)
+  (not (zero? (mod nm dv))))
 
 (defn sieve [ls nm]
   (filter
     (fn [el] (notMult el nm))
-    ls
-  )
-)
+    ls))
 
 (defn eratosphenes [candidates]
   (if (not (empty? candidates))
-    (
-      (def curPrime (first candidates))
-      (println curPrime)
-      (eratosphenes (sieve candidates curPrime))
-    )
-  )
-)
+    ((def curPrime (first candidates))
+     (println curPrime)
+     (eratosphenes (sieve candidates curPrime)))))
 
 (eratosphenes nums)
