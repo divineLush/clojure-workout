213439f696c7981ff787241ecd60d867ce4b13b1
divineLush <alvipath@yahoo.com>
2022-03-20 06:08:32 -0400

    substrings-basic fixes

--- substrings-basic.clj
+++ substrings-basic.clj
@@ -1,35 +1,37 @@
 (println "pls type smth")
 (def alphabet (str (read)))
 
-(def alphabet-len (count alphabet))
-; (println "pls input n")
-; (def n (int (read)))
+(println "pls input n")
+(def n (int (read)))
 
 (defn is-last-equal [x y]
-  (= (str (last x)) (str y)))
+  (= (str (last (str x))) (str y)))
 
 (defn smart-conj [s x y]
-  (if (is-last-equal x y) s (conj s (str x y))))
+  (if (is-last-equal x y)
+    s
+    (conj s (str x y))))
 
 (defn build-seq
-  ([msg] (build-seq msg '[] 0))
-  ([msg cur-seq i]
-    (if (= i alphabet-len)
+  ([msg] (build-seq msg '[] alphabet))
+  ([msg cur-seq alph-seq]
+    (if (empty? alph-seq)
       cur-seq
-      (recur msg (smart-conj cur-seq msg (nth alphabet i)) (inc i)))))
+      (recur msg (smart-conj cur-seq msg (first alph-seq)) (rest alph-seq)))))
 
 (defn concat-seqs
-  ([msg] (concat-seqs (str msg) '[] 0))
-  ([msg cur-seq i]
-    (if (= i alphabet-len)
+  ([inp-seq] (concat-seqs inp-seq '[]))
+  ([inp-seq cur-seq]
+    (if (empty? inp-seq)
       cur-seq
-      (recur msg (concat cur-seq (build-seq msg)) (inc i)))))
+      (recur (rest inp-seq) (concat cur-seq (build-seq (first inp-seq)))))))
 
-; (defn iter
-;   ([] (iter '[] 0))
-;   ([cur-seq i]
-;     (if (= i alphabet-len)
-;       cur-seq
-;       (recur (concat cur-seq (concat-seqs (nth alphabet i))) (inc i)))))
+(defn gen-seq
+  ([] (gen-seq n alphabet))
+  ([i cur-seq]
+    (if (= i 1)
+      cur-seq
+      (recur (dec i) (concat-seqs cur-seq)))))
 
-(println (concat-seqs alphabet))
+(if (> n 1)
+  (println (gen-seq)))
