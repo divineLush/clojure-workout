3680f8f7a0833707ea31588b687f6ea6d7331bf9
divineLush <alvipath@yahoo.com>
2022-04-04 06:19:40 -0400

    refactor

--- endless-primes.clj
+++ endless-primes.clj
@@ -2,11 +2,15 @@
 (def n (int (read)))
 
 (def primes
-  (letfn [(primes-parts [part]
+  (letfn [(primes-chunks [p-chunk]
     (lazy-seq
-      (when-let [[p & xs] (seq part)]
-        (let [cant-div #(not(zero? (mod % p)))]
-          (cons p (primes-parts (filter cant-div xs)))))))]
-            (primes-parts (iterate inc 2))))
+      (when-let [[p & xs] (seq p-chunk)]
+        (let [not-mult (fn [inp] (not(zero? (mod inp p))))]
+          (cons p (primes-chunks (filter not-mult xs)))))))]
+    (primes-chunks (iterate inc 2))))
 
-(println (nth primes n))
+(if (>= n 0)
+  (println (nth primes n)))
+
+; (if (>= n 0)
+;   (println (take n primes)))
