cf8244debcf217ba0306d8b838b9c521172c27d5
divineLush <alvipath@yahoo.com>
2022-04-23 12:15:08 -0400

    integral

---
+++ integral.clj
@@ -0,0 +1,22 @@
+(println "pls type x")
+(def x (int (read)))
+
+(defn input-fn [a] (/ 1 (Math/log a)))
+(def mem-input-fn (memoize input-fn))
+
+(def steps-num 15)
+(def step (float (/ x steps-num)))
+
+(def points (range 0 x step))
+(def values (map mem-input-fn points))
+
+(def partial-sum
+  (reduce + (rest (drop-last values))))
+
+(def first-and-last
+  (float (/ (+ (first values) (last values)) 2)))
+
+(def res
+  (* step (+ first-and-last partial-sum)))
+
+(println res)
